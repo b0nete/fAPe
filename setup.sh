0281f4ba9b56334aca479cee876e88608683d50e
root <root@localhost.localdomain>
2017-03-23 00:07:21 -0300

    zurdo

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# Recomendamos actualizar sistema al usuario.
+echo "Antes de comenzar con la instalación de las dependencias, desea actualizar su sistema?."
+
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes ) sudo apt-get update & sudo apt-get upgrade; break;;
+		No ) break;;
+	esac
+done
+
+# Instalar dependencias.
+# isc-dhcp-server, hostapd
+echo "Desea instalar las dependencias necesitarias para utilizar fAPe?"
+
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes ) sudo apt-get install isc-dhcp-server &&
+			  sudo apt-get install hostapd; break;;
+		No ) break;;
+	esac
+done
